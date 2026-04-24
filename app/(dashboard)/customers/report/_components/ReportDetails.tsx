a5829e17e4ad291a868d7dd933bcf2d94099c467
hrid1 <hridoycse1@gmail.com>
2026-04-24 11:12:07 +0600

    move work from main to hrid

--- app/(dashboard)/customers/report/_components/ReportDetails.tsx
+++ app/(dashboard)/customers/report/_components/ReportDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReportsAccordion from './ReportsAccordion';
 
 export default function ReportDetails({ selectedId }: { selectedId: string | null }) {
 
@@ -8,7 +9,7 @@ export default function ReportDetails({ selectedId }: { selectedId: string | nul
 
             {/* info */}
             <div className='w-full '>
-                <h3 className='flex-[1_0_0] text-[color:var(--Stroke,#2A3542)] [font-family:Lora] text-lg font-bold leading-[130%] pb-2'>Customer Reports Details</h3>
+                <h3 className='flex-[1_0_0] text-[#2A3542] font-lora text-lg font-bold leading-[130%] pb-2'>Customer Reports Details</h3>
             </div>
 
             <div className='w-full  flex justify-between items-center'>
@@ -28,33 +29,56 @@ export default function ReportDetails({ selectedId }: { selectedId: string | nul
                 <button className='px-4 py-2 rounded-xl bg-red-500 text-white '>Suspend Customer</button>
             </div>
 
-            
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  w-full">
-                    <StatCard label="Completed Orders" value="3124" />
-                    <StatCard label="Cancelled Order" value="7" />
-                    <StatCard label="Incomplete Orders" value="37" />
-                    <StatCard label="Report Count" value="34" />
-                    <StatCard label="Vendor Count" value="7" />
-                    <StatCard label="Last Ordered" value="12-02-2026" />
+            <div  className='w-full space-y-2  '>
+                <div className=' w-full'>
+                    <div className='flex items-center justify- gap-6 bg-[#F6F8FA] px-4 py-3 rounded-lg'>
+                        {/* First Item */}
+                        <StatCard label="Completed Orders" value="3124" />
+                        {/* Second Item */}
+                        <StatCard label="Completed Orders" value="3124" />
+                        {/* Third Item */}
+                        <StatCard label="Completed Orders" value="3124" isLast />
+
+                    </div>
                 </div>
-           
+                <div className=' w-full'>
+                    <div className='flex items-center justify- gap-6 bg-[#F6F8FA] px-4 py-3 rounded-lg'>
+                        {/* First Item */}
+                        <StatCard label="Completed Orders" value="3124" />
+                        {/* Second Item */}
+                        <StatCard label="Completed Orders" value="3124" />
+                        {/* Third Item */}
+                        <StatCard label="Completed Orders" value="3124" isLast />
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
 
             <div className='flex flex-col gap-4'>
                 <div className='flex flex-col gap-2'>
                     <h1 className='text-2xl font-bold'>Report Details for {selectedId}</h1>
                 </div>
             </div>
+
+
+            <ReportsAccordion/>
         </div>
     )
 }
 
 
 // Reusable card for a single stat
-function StatCard({ label, value }: { label: string; value: string | number }) {
+function StatCard({ label, value, isLast = false }: { label: string; value: string | number, isLast?: boolean }) {
     return (
-        <div className="bg-white p-2 border border-[#ECEFF3] rounded-2xl shadow-sm">
-            <p className="h-5 self-stretch text-[color:var(--Secondary-Text,#697586)] [font-family:Inter] text-xs font-normal leading-[120%] ">{label}</p>
+        <div className={`${isLast ? '' : 'pr-6 border-r border-gray-300'}`}>
+            <p className="min-h-5 self-stretch text-[color:var(--Secondary-Text,#697586)] [font-family:Inter] text-xs font-normal leading-[120%] ">{label}</p>
             <p className="text-[color:var(--Stroke,#2A3542)] [font-family:Lora] text-base font-bold leading-[130%]">{value}</p>
         </div>
     );
 }
+
+
