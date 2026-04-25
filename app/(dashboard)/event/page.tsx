1949de89a30728ccf5dc5a945ccf422fbf772b87
hrid1 <hridoycse1@gmail.com>
2026-04-25 12:32:44 +0600

    Update not found page

--- app/(dashboard)/event/page.tsx
+++ app/(dashboard)/event/page.tsx
@@ -1,3 +1,17 @@
-export default function eventPage() {
-  return <h2>this is event page</h2>;
+import Image from 'next/image'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+
+
+      <Image src="/not-available.svg" alt="event-bg" width={1000} height={1000} />
+
+      <div className='max-w-[760px] mx-auto'>
+        <h2 className='self-stretch text-[#2A3542] text-center [font-family:Lora] text-[40px] font-bold leading-[130%] tracking-[1.6px]'>This feature will be available in the beta version.</h2>
+        <p className='mt-1 self-stretch text-[#697586] text-center  text-lg font-medium leading-[160%]'>Stay tuned for advanced event curation and logistical management tools designed for high-stakes environments. Beta version is underway, and access will be available soon.</p>
+      </div>
+    </div>
+  )
 }
